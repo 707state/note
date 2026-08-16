47a79d6e47b7decf7863d2a5f7c8ef3de5c8b10f
jask <2713427119@qq.com>
2026-08-16 20:24:21 +0800

    Use canvas demo

--- main.cc
+++ main.cc
@@ -1,84 +1,51 @@
-#include <ftxui/component/component.hpp>
-#include <ftxui/component/screen_interactive.hpp>
-#include <ftxui/dom/elements.hpp>
 
-using namespace ftxui;
+// Copyright 2020 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+#include <stdio.h>                 // for getchar
+#include <cmath>                   // for cos
+#include <ftxui/dom/elements.hpp>  // for Fit, canvas, operator|, border, Element
+#include <ftxui/screen/screen.hpp>  // for Cell, Screen
+#include <vector>                   // for vector, allocator
+
+#include "ftxui/dom/canvas.hpp"  // for Canvas
+#include "ftxui/dom/node.hpp"    // for Render
+#include "ftxui/screen/color.hpp"  // for Color, Color::Red, Color::Blue, Color::Green, ftxui
 
 int main() {
-  // State
-  int counter = 0;
-  int slider_value = 50;
-  bool checked = false;
-  std::vector<std::string> menu_entries = {"Option A", "Option B", "Option C"};
-  int menu_selected = 0;
+  using namespace ftxui;
 
-  // Components
-  auto slider = Slider("Slider:", &slider_value, 0, 100, 1);
+  auto c = Canvas(100, 100);
 
-  auto checkbox = Checkbox("Enable feature", &checked);
-
-  auto menu = Menu(&menu_entries, &menu_selected);
-
-  auto button_dec = Button("Decrement", [&] { --counter; });
-  auto button_inc = Button("Increment", [&] { ++counter; });
-  auto button_reset = Button("Reset", [&] { counter = 0; });
-
-  auto buttons = Container::Horizontal({button_dec, button_inc, button_reset});
-
-  auto left_panel = Container::Vertical({menu, checkbox});
-
-  auto right_panel = Container::Vertical({
-      buttons,
-      slider,
+  c.DrawText(0, 0, "This is a canvas", [](Cell& p) {
+    p.foreground_color = Color::Red;
+    p.underlined = true;
   });
 
-  auto main_container = Container::Horizontal({left_panel, right_panel});
+  // Triangle:
+  c.DrawPointLine(10, 10, 80, 10, Color::Red);
+  c.DrawPointLine(80, 10, 80, 40, Color::Blue);
+  c.DrawPointLine(80, 40, 10, 10, Color::Green);
 
-  // Renderer
-  auto renderer = Renderer(main_container, [&] {
-    return vbox({
-        text(" FTXUI Demo ") | bold | center | border,
-        separator(),
-        hbox({
-            vbox({
-                text(" Menu ") | bold,
-                menu->Render() | border | size(WIDTH, GREATER_THAN, 20),
-                separator(),
-                text(" Toggle ") | bold,
-                checkbox->Render(),
-            }) | flex,
-            separator(),
-            vbox({
-                text(" Counter ") | bold,
-                hbox({
-                    text("  Value: "),
-                    text(std::to_string(counter)) | bold | color(Color::Cyan),
-                }),
-                buttons->Render(),
-                separator(),
-                text(" Slider ") | bold,
-                slider->Render() | size(WIDTH, GREATER_THAN, 30),
-            }) | flex,
-        }) | flex,
-        separator(),
-        hbox({
-            text(" Menu: ") | dim,
-            text(menu_entries[menu_selected]) | color(Color::Yellow),
-            filler(),
-            text(" Slider: ") | dim,
-            text(std::to_string(slider_value) + "%") | color(Color::Green),
-            filler(),
-            text(" Checkbox: ") | dim,
-            text(checked ? "ON" : "OFF") | color(checked ? Color::Green : Color::Red),
-            filler(),
-            text(" Ctrl+C to quit ") | dim | center,
-        }),
-    }) |
-    border | center;
-  });
+  // Circle, not filled and filled:
+  c.DrawPointCircle(30, 50, 20);
+  c.DrawPointCircleFilled(40, 40, 10);
 
-  auto screen = ScreenInteractive::Fullscreen();
-  screen.Loop(renderer);
+  // Plot a function:
+  std::vector<int> ys(100);
+  for (int x = 0; x < 100; x++) {
+    ys[x] = int(80 + 20 * cos(x * 0.2));
+  }
+  for (int x = 0; x < 99; x++) {
+    c.DrawPointLine(x, ys[x], x + 1, ys[x + 1], Color::Red);
+  }
+
+  auto document = canvas(&c) | border;
+
+  auto screen = Screen::Create(Dimension::Fit(document));
+  Render(screen, document);
+  screen.Print();
+  getchar();
 
   return 0;
 }
